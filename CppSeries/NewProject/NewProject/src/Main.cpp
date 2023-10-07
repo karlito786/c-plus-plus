2a8a37434fa0f227e55b007f1b96abfd312e2051
karlito786 <119797568+karlito786@users.noreply.github.com>
2023-10-07 23:31:16 +0200

    Add files via upload

--- CppSeries/NewProject/NewProject/src/Main.cpp
+++ CppSeries/NewProject/NewProject/src/Main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#define LOG(x) std::cout << x << std::endl
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	// void* ptr = 0;
+	// void* ptr = NULL;
+	// void* ptr = nullptr;
+	int var = 8;
+	int* ptr = &var; 
+	*ptr = 10;
+	LOG(var);
+	std::cin.get();
 }
